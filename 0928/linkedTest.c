b6df1f9d42ad2d720a7223914fd9f3c74e8f5232
chichoon <chichoon.choi@gmail.com>
2021-10-02 22:17:13 +0900

    [~1005] ADD: 링크드리스트 테스트함수

---
+++ 0928/linkedTest.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkedlist.h"
+
+void	printLinkedList(LinkedList *pList)
+{
+	int			i;
+	ListNode	*node;
+
+	i = -1;
+	node = &pList->headerNode;
+	while (++i < pList->currentElementCount)
+	{
+		node = node->pLink;
+		printf("%d ", node->data);
+	}
+	printf("\n");
+}
+
+int	main(int argc, char *argv[])
+{
+	int			i;
+	LinkedList	*pList;
+	ListNode	node;
+
+	pList = createLinkedList();
+	if (pList != NULL)
+	{
+		node.data = 5;
+		addLLElement(pList, 1, node);
+		node.data = 7;
+		addLLElement(pList, 1, node);
+		node.data = 3;
+		addLLElement(pList, 1, node);
+		printLinkedList(pList);
+	}
+}
