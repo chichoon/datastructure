edc6526f8536cf31cbf65e95784cb46933a7258b
chichoon <chichoon.choi@gmail.com>
2021-10-09 17:17:57 +0900

    [~1012] ADD: Stack 구현을 위한 array, linked 함수 불러오기

---
+++ 03/stack.h
@@ -0,0 +1,21 @@
+#ifndef _STACK_
+#define _STACK_
+
+#include "arraylist/arraylist.h"
+#include "linkedlist/linkedlist.h"
+
+typedef struct LinkedStackType
+{
+	int currentElementCount;	// 현재 원소의 개수
+	StackNode* pTopElement;		// Top 노드의 포인터
+} LinkedStack;
+
+LinkedStack* createLinkedStack();
+int pushLS(LinkedStack* pStack, StackNode element);
+StackNode* popLS(LinkedStack* pStack);
+StackNode* peekLS(LinkedStack* pStack);
+void deleteLinkedStack(LinkedStack* pStack);
+int isLinkedStackFull(LinkedStack* pStack);
+int isLinkedStackEmpty(LinkedStack* pStack);
+
+#endif
