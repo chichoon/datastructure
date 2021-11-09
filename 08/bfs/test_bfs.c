f5f3d4c2cb381125d6d618dc1d21a72d90382f46
chichoon <chichoon.choi@gmail.com>
2021-11-09 22:42:54 +0900

    [~1112] UPDATE: bfs 테스트코드 구현 완료

--- 08/bfs/test_bfs.c
+++ 08/bfs/test_bfs.c
@@ -4,15 +4,22 @@ int	main()
 {
 	ArrayGraph *graph;
 
-	graph = createArrayGraph(6);
+	graph = createArrayGraph(15);
 	for (int i = 0; i < 6; i++) {
-		addVertexAG(graph, i);
+		addVertexAG(graph, 2 * i + 1);
 	}
-	addEdgeAG(graph, 0, 1);
-	addEdgeAG(graph, 0, 2);
+	addEdgeAG(graph, 1, 5);
 	addEdgeAG(graph, 1, 3);
-	addEdgeAG(graph, 2, 4);
-	addEdgeAG(graph, 2, 5);
+	addEdgeAG(graph, 5, 9);
+	addEdgeAG(graph, 3, 11);
+	addEdgeAG(graph, 3, 7);
+	/*
+			  1
+			/  \
+			5  3
+		   /  /\
+		  9  11 7
+	*/
 	displayArrayGraph(graph);
 
 	bfs(graph);
