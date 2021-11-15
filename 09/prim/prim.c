af37b8055817be6a6b91c05de76343977e80d18c
chichoon <chichoon.choi@gmail.com>
2021-11-16 01:38:25 +0900

    [~1116] UPDATE: prim 알고리즘 끝

--- 09/prim/prim.c
+++ 09/prim/prim.c
@@ -8,75 +8,67 @@ int	ifTraversedAll(ArrayGraph *graph, int *vertexArray)
 	return (TRUE);
 }
 
-int	findStartPoint(ArrayGraph *graph)
-{
-	int	startPoint = -1;
-
-	for (int i = 0; i < graph->maxVertexCount; i++)
-	{
-		if (graph->pVertex[i] == USED)
-		{
-			startPoint = i;
-			break ;
-		}
-	}
-	return (startPoint);
-}
-
-int	findIfCircular(ArrayGraph *graph, int startNode)
-{
-	ArrayStack	*stack;
-	StackNode	element;
-	int			now;
-	int			*vertexArray;
-
-	stack = createArrayStack(graph->maxVertexCount);
-	vertexArray = malloc(sizeof(int) * graph->maxVertexCount);
-	if (!stack || !vertexArray)
-		return (-1);
-	memset(vertexArray, FALSE, sizeof(int) * graph->maxVertexCount);
-	vertexArray[startNode] = TRUE;
-	element.data = startNode;
-	pushAS(stack, element);
-	while (!isArrayStackEmpty(stack))
-	{
-		now = stackpop(arr);
-
-	}
-}
-
 ArrayGraph	*prim(ArrayGraph *originalGraph)
 {
-	ArrayGraph	*returnGraph;
-	int			*vertexArray;
-	int			v = -1;
-	int			minWeight;
+	ArrayGraph		*returnGraph;
+	int				*vertexArray;
+	int				minWeight;
+	int				vertexTemp;
 
-	//Array, Graph 초기화
+	//Array, Graph, Queue 생성
 	returnGraph = createArrayGraph(originalGraph->maxVertexCount);
 	vertexArray = malloc(sizeof(int) * originalGraph->maxVertexCount);
 	if (!returnGraph || !vertexArray)
 		return (NULL);
+	//Array, Graph, Queue 초기화
 	memset(vertexArray, FALSE, sizeof(int) * originalGraph->maxVertexCount);
-	//Graph의 Vertex중 첫번째로 등장하는 Vertex를 시작점으로 삼음
-	v = findStartPoint(originalGraph);
-	if (v < 0)
-		return (NULL);
-	vertexArray[v] = TRUE;
-	//Prim 알고리즘 시작
-	for (int j = 0; j < originalGraph->maxVertexCount; j++)
+	for (int i = 0; i < originalGraph->maxVertexCount; i++)
+		if (originalGraph->pVertex[i] == USED)
+			addVertexAG(returnGraph, i);
+	//시작점 방문 체크
+	vertexArray[0] = VISITED;
+	for (int i = 0; i < originalGraph->maxVertexCount; i++)
 	{
-		minWeight = INT32_MAX;
-		for (int i = 0; i < originalGraph->maxVertexCount; i++)
+		minWeight = INF;
+		for (int j = 0; j < originalGraph->maxVertexCount; j++)
 		{
-			if (
-				originalGraph->ppAdjEdge[v][i] > 0
-				&& originalGraph->ppAdjEdge[v][i] < minWeight
-				&& vertexArray[i] == NOT_USED
-				)
+			if (!vertexArray[j]
+				&& originalGraph->ppAdjEdge[i][j] > 0
+				&& minWeight > originalGraph->ppAdjEdge[i][j] )
 			{
-
+				minWeight = originalGraph->ppAdjEdge[i][j];
+				vertexTemp = j;
 			}
 		}
+		addEdgewithWeightAG(returnGraph, i, vertexTemp, minWeight);
 	}
+	return (returnGraph);
+}
+
+int main(void)
+{
+	ArrayGraph *graph;
+	ArrayGraph *mst;
+
+	graph = createArrayGraph(6);
+	if (!graph)
+		return (0);
+	for (int i = 0; i < 6; i++)
+		addVertexAG(graph, i);
+	addEdgewithWeightAG(graph, 0, 1, 4);
+	addEdgewithWeightAG(graph, 0, 2, 3);
+	addEdgewithWeightAG(graph, 1, 2, 2);
+	addEdgewithWeightAG(graph, 2, 3, 1);
+	addEdgewithWeightAG(graph, 3, 4, 1);
+	addEdgewithWeightAG(graph, 3, 5, 5);
+	addEdgewithWeightAG(graph, 4, 5, 6);
+	mst = prim(graph);
+	if (!mst)
+		return (0);
+	for (int i = 0; i < 6; i++)
+		for (int j = 0; j < 6; j++)
+			if (i < j && mst->ppAdjEdge[i][j] > 0)
+				printf("%d to %d : %d\n", i, j, mst->ppAdjEdge[i][j]);
+	deleteArrayGraph(graph);
+	deleteArrayGraph(mst);
 }
--- 09/prim/prim.h
+++ 09/prim/prim.h
@@ -2,10 +2,13 @@
 # define PRIM_H
 
 # include "arraygraph.h"
-# include "arraystack.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 
+#define VISITED				1
+#define NOT_VISITED			0
+
+#define INF	1000000
 
 #endif
