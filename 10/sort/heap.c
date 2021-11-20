7b6aedfaf25bf6e596076bcbd7d0011085d9feef
chichoon <chichoon.choi@gmail.com>
2021-11-20 15:56:54 +0900

    [~1123] ADD: heap 추가 (임시)

---
+++ 10/sort/heap.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+void upheap(int* a, int k) {
+	int v;
+	v = a[k];
+	a[0] = INT_MAX;
+	while (a[k / 2] <= v) {
+		a[k] = a[k / 2];
+		k /= 2;
+	}
+	a[k] = v;
+}
+
+void downheap(int* a, int N, int k) {
+	int i, v;
+	v = a[k];
+	while (k <= N / 2) {
+		i = k << 1;
+		if (i < N && a[i] < a[i + 1]) i++;
+		if (v >= a[i]) break;
+		a[k] = a[i];
+		k = i;
+	}
+	a[k] = v;
+}
+
+void insert(int* a, int* N, int v) {
+	a[++(*N)] = v;
+	upheap(a, *N);
+}
+
+int extract(int* a, int* N) {
+	int v = a[1];
+	a[1] = a[(*N)--];
+	downheap(a, *N, 1);
+
+	return v;
+}
+
+void heap_sort(int* a, int N) {
+	int i;
+	int hn = 0;
+
+	for (i = 1; i <= N; i++)
+		insert(a, &hn, a[i]);
+	for (i = hn; i >= 1; i--)
+		a[i] = extract(a, &hn);
+}
